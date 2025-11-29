8697098d5b6a3ce19ff61871917c11804e4acbfb
RispaJoseph <rizpahjoseph@gmail.com>
2025-11-29 21:33:33 +0530

    frontend folder setup added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <div className="p-6 bg-white rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold text-indigo-600">Tailwind is working ✅</h1>
+      </div>
+    </div>
+  );
+}
+
