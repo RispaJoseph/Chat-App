0382257b4afc8a0f250b602fb3bbc94ecdfb439b
RispaJoseph <rizpahjoseph@gmail.com>
2025-12-03 21:31:32 +0530

    Registers the models in Django admin

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Room, Message
 
-# Register your models here.
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "created_by", "created_at")
+    search_fields = ("name", "slug")
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("user", "room", "text", "timestamp")
+    list_filter = ("room", "user")
+    search_fields = ("text,")
+
