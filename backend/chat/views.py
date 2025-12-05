9b1bf350ec88f1856162431f5c140329655ab7c7
RispaJoseph <rizpahjoseph@gmail.com>
2025-12-05 12:46:20 +0530

    views for chat app added

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Room, Message
+from .serializers import RoomSerializer, MessageSerializer
 
-# Create your views here.
+class RoomListCreateView(generics.ListCreateAPIView):
+    queryset = Room.objects.all().order_by("-created_at")
+    serializer_class = RoomSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+class MessageListView(generics.ListAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        slug = self.kwargs["slug"]
+        return Message.objects.filter(room__slug=slug).order_by("timestamp")
