b8d59e3a4cbfafbbb2248f138fcce5533963bf01
RispaJoseph <rizpahjoseph@gmail.com>
2025-12-30 23:04:40 +0530

    updated

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -8,8 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR.parent / ".env")
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "dev-secret-key")
